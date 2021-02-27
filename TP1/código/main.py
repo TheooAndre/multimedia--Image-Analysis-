49cdb7ad440b51acb29af521cc9b21d9f451dc88
Marcelo Cruz <zemarcelocruz@gmail.com>
2021-02-27 13:41:42 +0000

    Sincronização

--- TP1/código/main.py
+++ TP1/código/main.py
@@ -33,6 +33,7 @@ def ex2():
 def view(colors, name, image):
     colormap = clr.LinearSegmentedColormap.from_list(name, colors, N=256)
     plt.figure()
+    plt.axis('off')
     plt.imshow(image, cmap = colormap)
 
 
@@ -52,21 +53,15 @@ def ex3():
     colors_green = [(0,0,0), (0,1,0)]
     colors_blue = [(0,0,0), (0,0,1)]
 
-    plt.figure()
-    plt.imshow(img)
-    view(colors_red, 'red', R)
-    view(colors_green, 'green', G)
-    view(colors_blue, 'blue', B)
-
     Y, Cb, Cr = RGB2YCbCr(R, G, B)
 
     view(colors_grayscale, 'grayscale', Y)
     view(colors_grayscale, 'blue', Cb)
     view(colors_grayscale, 'red', Cr)
 
-    R2, G2, B2 = YCbCr2RGB(Y, Cb, Cr);
-   #testar a conversao se deu os valores originais
-    print(R2 == R)
+    R2, G2, B2 = YCbCr2RGB(Y, Cb, Cr)
+    #testar a conversao se deu os valores originais
+    #print(R2 == R)
     view(colors_red, 'red', R2)
     view(colors_green, 'green', G2)
     view(colors_blue, 'blue', B2)
@@ -74,9 +69,6 @@ def ex3():
     plt.show()
 
 def YCbCr2RGB(Y, Cb, Cr):
-    #R = Y + 1.402*(Cr - 128)
-    #G = Y - 0.344136*(Cb - 128) - 0.714136*(Cr - 128)
-    #B = Y + 1.772*(Cb - 128)
     height, width = Y.shape
     R = np.empty((height, width), dtype = np.uint8)
     G = np.empty((height, width), dtype = np.uint8)
@@ -102,9 +94,6 @@ def YCbCr2RGB(Y, Cb, Cr):
     return R, G, B
 
 def RGB2YCbCr(R, G, B):
-    #Y = 0.299*R + 0.587*G + 0.114*B
-    #Cb = (B-Y)/1.772 + 128
-    #Cr = (R-Y)/1.402 + 128
     height, width = R.shape
     Y = np.empty((height, width), dtype = np.uint8)
     Cb = np.empty((height, width), dtype = np.uint8)
@@ -117,25 +106,26 @@ def RGB2YCbCr(R, G, B):
     return Y, Cb, Cr
 
 def ex4():
-    img, R, G, B = open_image('../imagens/peppers.bmp')
-    #img = plt.imread('../imagens/peppers.bmp')
-    print("Dimensões originais:", img.shape)
-    
-    ds = '4:2:0'
-    
-    Y, Cb, Cr = RGB2YCbCr(R, G, B);
-    
-    Y, Cb_d, Cr_d = downsample(Y, Cb, Cr, ds)
-   
-    print("Dimensões Y:", Y.shape)
-    print("Dimensões Cb_d:", Cb_d.shape)
-    print("Dimensões Cr_d:", Cr_d.shape)
-    
-    colors_grayscale = [(0,0,0), (0.5,0.5,0.5)]
-    
-        
-    plt.show()
-    
+	img, R, G, B = open_image('../imagens/peppers.bmp')
+	print("Dimensões originais:", img.shape)
+	
+	ds = '4:2:0'
+	
+	Y, Cb, Cr = RGB2YCbCr(R, G, B)
+	
+	Y, Cb_d, Cr_d = downsample(Y, Cb, Cr, ds)
+	
+	print("Dimensões Y:", Y.shape)
+	print("Dimensões Cb_d:", Cb_d.shape)
+	print("Dimensões Cr_d:", Cr_d.shape)
+	
+	colors_grayscale = [(0,0,0), (0.5,0.5,0.5)]
+	view(colors_grayscale, 'grayscale', Y)
+	view(colors_grayscale, 'grayscale', Cb_d)
+	view(colors_grayscale, 'grayscale', Cr_d)
+		
+	plt.show()
+	
 def downsample(C1, C2, C3, d):
     height, width = C1.shape
     if d == '4:2:0':
@@ -148,7 +138,7 @@ def downsample(C1, C2, C3, d):
         pass
 
     return C1, C2, C3
-   
+    
 def upsample(C1, C2, C3, d, filt=True):
     if d == '4:2:0':
         C2_us = np.repeat(C2, 2, axis=1)
